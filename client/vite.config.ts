502b88718b09b415cdbd0cd705c8fcd48fd9c1d8
romaisa02 <iqbalromaisa618@gmail.com>
2026-08-12 20:47:15 +0400

    added server url and remove direct link

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const target = env.VITE_SERVER_ORIGIN || 'http://localhost:5001';
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
